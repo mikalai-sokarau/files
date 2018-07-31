e110ff4e3d538168d2d86657f8850d937ba7ef53
Mikk <mikalaisokarau@gmail.com>
2018-07-31 22:09:54 +0300

    started hw04

--- http-servers/json-server.js
+++ http-servers/json-server.js
@@ -1,11 +1,23 @@
 const http = require('http');
 
+const product = {
+  id: 1,
+  name: 'Supreme T-Shirt',
+  brand: 'Supreme',
+  price: 99.99,
+  options: [
+      { color: 'blue' },
+      { size: 'XL' }
+  ]
+};
+
 const app = http.createServer((req, res) => {
-  res.end('Hello, world');
+  res.setHeader('Content-Type', 'application/json');
+  res.statusCode = 200;
+  
+  res.write(JSON.stringify(product));
+  res.end();
 });
 app.listen(3000);
 
-res
-
-asyncronous module api 7 lecture
 
