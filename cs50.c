51968cc121f8fc3e228798726f6683c93aef92d8
Mikk <mikalaisokarau@gmail.com>
2018-08-04 18:25:58 +0300

    added solution

--- cs50.c
+++ cs50.c
@@ -1,40 +1,81 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
-bool check_card_number(int length, int first_sign)
-{
-  bool is_american_express = length == 15 && (first_sign == 34 || first_sign == 37);
-  bool is_visa = (length == 13 || length == 16) && first_sign == 4;
-  bool is_master_card = length == 16 && (first_sign >= 51 && first_sign <= 55);
-
-  printf("is_a_e: %d\n, is_v: %d\n, is_m_c: %d\n", is_american_express, is_visa, is_master_card);
-  return is_american_express || is_visa || is_master_card;
-}
+void check_card_number(int length, int first_signs);
+char card_name[] = "INVALID";
 
 int main(void)
 {
   long long card_number;
+  int first_signs;
   int length;
-  int first_sign;
+  int result = 0;
+  long long user_input;
 
   do
   {
-    length = 0;
-    long long user_input = get_long_long("Enter card number: ");
+    user_input = get_long_long("Enter card number: ");
     card_number = user_input;
+    length = 0;
 
     while (user_input)
     {
-      first_sign = user_input;
+      if (user_input <= 99 && user_input >= 10)
+      {
+        first_signs = user_input;
+      }
       user_input /= 10;
       length++;
     }
-  } while (check_card_number(length, first_sign));
+  } while (user_input < 0);
 
-  printf("%lld\n", card_number);
+  check_card_number(length, first_signs);
+
+      for (int i = 0; i < length; i++)
+  {
+    int last_number = card_number % 10;
+
+    if (i % 2 == 0)
+    {
+      result += last_number;
+    }
+    else
+    {
+      int multiplied_number = last_number * 2;
+      result += multiplied_number > 9 ? (multiplied_number % 10) + 1 : multiplied_number;
+    }
+    card_number /= 10;
+  }
+
+  if (result % 10)
+  {
+    printf("%s\n", "INVALID");
+  }
+  else
+  {
+    printf("%s\n", card_name);
+  }
 
   return 0;
 }
 
-confirmation modal сделать
+void check_card_number(int length, int first_signs)
+{
+  bool is_american_express = length == 15 && (first_signs == 34 || first_signs == 37);
+  bool is_visa = (length == 13 || length == 16) && (first_signs >= 40 && first_signs <= 49);
+  bool is_master_card = length == 16 && (first_signs >= 51 && first_signs <= 55);
 
+  if (is_american_express)
+  {
+    strcpy(card_name, "AMEX");
+  }
+  else if (is_visa)
+  {
+    strcpy(card_name, "VISA");
+  }
+  else if (is_master_card)
+  {
+    strcpy(card_name, "MASTERCARD");
+  }
+}
